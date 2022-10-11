71db35037507512100d8199e70182e8d8f8b2b1c
Lucas Tavares <lucastavaresvasconcelos@gmail.com>
2022-10-11 15:43:22 -0300

    ➕ [FEATURE] - Adding some style in foods card

--- Study-SwiftUi/Views/ExampleView.swift
+++ Study-SwiftUi/Views/ExampleView.swift
@@ -12,36 +12,45 @@ struct ExampleView: View {
     @State var food: Food?
     
     var body: some View {
-        VStack {
-            if let food = food {
-                AsyncImage(url: URL(string: food.image)) { image in
-                    image
-                        .resizable()
-                        .frame(width: 200, height: 200)
-                } placeholder: {
-                    ProgressView()
-                }
-            } else {
-                ProgressView()
-                    .padding()
-                    .onAppear() {
-                        FoodApi().loadData { food in
-                            self.food = food
-                        }
+        GeometryReader { metrics in
+            VStack {
+                if let food = food {
+                    AsyncImage(url: URL(string: food.image)) { image in
+                        image
+                            .resizable()
+                            .padding()
+                            .frame(width: metrics.size.width, height: metrics.size.height * 0.6)
+                                
+                    } placeholder: {
+                        ProgressView()
                     }
-            }
-            Spacer()
-            Button("Another dish") {
-                FoodApi().loadData { food in
-                    self.food = food
+                } else {
+                    ProgressView()
+                        .padding()
+                        .onAppear() {
+                            FoodApi().loadData { food in
+                                self.food = food
+                            }
+                        }
                 }
+                Button("Another dish") {
+                    FoodApi().loadData { food in
+                        self.food = food
+                    }
+                }
+                .padding()
+                .font(.headline)
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .clipShape(Capsule())
             }
         }
+        
     }
 }
 
 struct ExampleView_Previews: PreviewProvider {
     static var previews: some View {
-        ExampleView()
+        ExampleView(food: Food(image: "https://foodish-api.herokuapp.com/images/biryani/biryani2.jpg"))
     }
 }
